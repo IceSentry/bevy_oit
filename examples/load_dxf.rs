c64d418d5388ac0b22d73d3be540e99cfe16c55b
IceSentry <c.giguere42@gmail.com>
2023-08-18 20:10:09 -0400

    better load_dxf example

--- examples/load_dxf.rs
+++ examples/load_dxf.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, io::BufReader, iter::Peekable, slice::Iter};
 
 use bevy::{
+    gizmos::GizmoPlugin,
     math::DVec3,
     prelude::*,
     reflect::{TypePath, TypeUuid},
@@ -8,13 +9,14 @@ use bevy::{
         mesh::{Indices, VertexAttributeValues},
         render_resource::{AsBindGroup, PrimitiveTopology, ShaderRef, TextureUsages},
     },
-    window::{PresentMode, WindowResolution},
+    window::WindowResolution,
 };
 use bevy_oit::{OitCamera, OitMaterial, OitMaterialMeshBundle, OitPlugin};
 use camera_controller::{CameraController, CameraControllerPlugin};
 use dxf::entities::EntityType;
 use nalgebra::{Isometry3, Point3};
 use rand::Rng;
+use skybox::{Skybox, SkyboxPlugin, SkyboxSettings};
 
 mod camera_controller;
 
@@ -29,7 +31,6 @@ fn main() {
                         bevy_oit::WINDOW_HEIGHT as f32,
                     )
                     .with_scale_factor_override(1.0),
-                    // present_mode: PresentMode::AutoNoVsync,
                     ..default()
                 }),
                 ..default()
@@ -37,9 +38,19 @@ fn main() {
             MaterialPlugin::<GoochMaterial>::default(),
             CameraControllerPlugin,
             OitPlugin,
+            SkyboxPlugin,
         ))
+        .insert_resource(SkyboxSettings::DARK_GRAY)
         .add_systems(Startup, setup)
         .add_systems(Update, toggle_material)
+        .insert_resource(GizmoConfig {
+            enabled: false,
+            aabb: AabbGizmoConfig {
+                draw_all: false,
+                ..default()
+            },
+            ..default()
+        })
         .run();
 }
 
@@ -49,7 +60,8 @@ struct KeepMaterial;
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 200.0, 500.0),
+            transform: Transform::from_xyz(250.0, 75.0, 300.0)
+                .looking_at(Vec3::new(200.0, 0.0, 150.0), Vec3::Y),
             camera_3d: Camera3d {
                 depth_texture_usages: (TextureUsages::RENDER_ATTACHMENT
                     | TextureUsages::TEXTURE_BINDING)
@@ -60,6 +72,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
         },
         CameraController::default(),
         OitCamera,
+        Skybox,
     ));
 
     commands.spawn(TextBundle::from_section(
@@ -353,4 +366,234 @@ pub fn build_bevy_mesh(vtx: &[DVec3], idx: Option<&[[u32; 3]]>) -> Mesh {
     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::from(uvs));
     mesh.set_indices(Some(Indices::U32(indices)));
     mesh
+}
+
+mod skybox {
+    use std::ops::RangeInclusive;
+
+    use bevy::{
+        prelude::*,
+        render::{
+            mesh::VertexAttributeValues,
+            view::{Layer, NoFrustumCulling, RenderLayers},
+        },
+        transform::TransformSystem,
+        utils::{FloatOrd, HashMap},
+    };
+
+    #[derive(Debug, Clone, Default)]
+    pub struct SkyboxPlugin;
+    impl Plugin for SkyboxPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_systems(Startup, spawn_skybox)
+                .add_systems(Update, update_skybox)
+                .add_systems(
+                    PostUpdate,
+                    update_cameras.after(TransformSystem::TransformPropagate),
+                );
+        }
+    }
+
+    #[derive(Debug, Clone, Resource, PartialEq)]
+    pub struct SkyboxSettings {
+        pub sky: Color,
+        pub ground: Color,
+        /// The maximum range is `-1.0..1.0`: the gradient spans the entire height of the skybox. To
+        /// push the gradient down, so the sky is a solid color and the transition starts at the
+        /// horizon, use `-1.0..0.0`.
+        pub gradient_range: RangeInclusive<f32>,
+    }
+
+    impl SkyboxSettings {
+        pub const DARK_GRAY: Self = Self {
+            sky: Color::rgb(0.37, 0.37, 0.37),
+            ground: Color::rgb(0.2, 0.2, 0.2),
+            gradient_range: -0.75..=0.30,
+        };
+
+        pub const DARK_BLUE: Self = Self {
+            sky: Color::BLACK,
+            ground: Color::rgb(0.1, 0.1, 0.3),
+            gradient_range: -1.0..=-0.1,
+        };
+
+        pub const OVERCAST_SKY: Self = Self {
+            sky: Color::rgb(0.55, 0.67, 0.94),
+            ground: Color::rgb(0.78, 0.78, 0.78),
+            gradient_range: -0.7..=0.0,
+        };
+
+        pub const SHERBERT: Self = Self {
+            sky: Color::PINK,
+            ground: Color::GOLD,
+            gradient_range: -0.8..=0.8,
+        };
+    }
+
+    impl Default for SkyboxSettings {
+        fn default() -> Self {
+            Self::DARK_GRAY
+        }
+    }
+
+    pub const SKYBOX_RENDER_LAYER: Layer = 9;
+    pub const SKYBOX_CAM_ORDER: isize = -10_000;
+
+    /// Marker component to tag the skybox mesh
+    #[derive(Debug, Default, Clone, Copy, Component)]
+    pub struct SkyboxMesh;
+
+    /// Marks a camera that should have a skybox rendered.
+    #[derive(Debug, Default, Clone, Copy, Component)]
+    pub struct Skybox;
+
+    /// Marker component to tag camera used to render the skybox
+    #[derive(Debug, Clone, Component)]
+    pub struct SkyboxFollowCamera {
+        following: Entity,
+    }
+
+    fn spawn_skybox(
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<StandardMaterial>>,
+    ) {
+        println!("spawn skybox");
+        commands.spawn((
+            SkyboxMesh,
+            NoFrustumCulling,
+            PbrBundle {
+                mesh: meshes.add(
+                    Mesh::try_from(shape::UVSphere {
+                        sectors: 24,
+                        stacks: 64,
+                        radius: 1.0,
+                    })
+                    .unwrap(),
+                ),
+                material: materials.add(StandardMaterial {
+                    cull_mode: Some(bevy::render::render_resource::Face::Front),
+                    unlit: true,
+                    depth_bias: -1e5_f32,
+                    ..Color::WHITE.into() // vertex colors are multiplied by the base color
+                }),
+                transform: Transform::default()
+                    .with_scale(Vec3::splat(1e5_f32))
+                    .with_rotation(Quat::from_euler(
+                        EulerRot::XYZ,
+                        -std::f32::consts::FRAC_PI_2,
+                        0.0,
+                        0.0,
+                    )),
+                ..default()
+            },
+            // RenderLayers::layer(SKYBOX_RENDER_LAYER),
+        ));
+    }
+
+    pub fn update_skybox(
+        mut meshes: ResMut<Assets<Mesh>>,
+        settings: Res<SkyboxSettings>,
+        mut last_skybox_settings: Local<Option<SkyboxSettings>>,
+        skybox: Query<&Handle<Mesh>, With<SkyboxMesh>>,
+    ) {
+        // Only continue if we haven't already made a skybox with these settings
+        match last_skybox_settings.as_ref() {
+            Some(x) if x != settings.as_ref() => {}
+            None => {}
+            _ => return,
+        }
+
+        let Some(skybox_mesh) = meshes.get_mut(skybox.single()) else {
+            error!("failed to get sky mesh");
+            return
+        };
+
+        println!("update skybox");
+
+        if let Some(VertexAttributeValues::Float32x3(positions)) =
+            skybox_mesh.attribute(Mesh::ATTRIBUTE_POSITION)
+        {
+            let z_min = positions.iter().map(|p| FloatOrd(p[2])).min().map(|n| n.0);
+            let z_min = z_min.unwrap_or_default() + settings.gradient_range.start() + 1.0;
+            let z_max = positions.iter().map(|p| FloatOrd(p[2])).max().map(|n| n.0);
+            let z_max = z_max.unwrap_or_default() + settings.gradient_range.end() - 1.0;
+
+            let lerp = |s: f32| ((s - z_min) / (z_max - z_min)).clamp(0.0, 1.0);
+            let mix = |s: f32, c1: Color, c2: Color| {
+                let c1 = Vec4::from_array(c1.as_rgba_f32());
+                let c2 = Vec4::from_array(c2.as_rgba_f32());
+                let mixed = c1.lerp(c2, s);
+                Color::from(mixed)
+            };
+
+            let colors: Vec<[f32; 4]> = positions
+                .iter()
+                .map(|[_x, _y, z]| {
+                    mix(lerp(*z), settings.ground, settings.sky).as_linear_rgba_f32()
+                })
+                .collect();
+            skybox_mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+
+            *last_skybox_settings = Some(settings.to_owned());
+        }
+    }
+
+    pub fn update_cameras(
+        mut commands: Commands,
+        mut skybox_cameras: Query<(
+            Entity,
+            &SkyboxFollowCamera,
+            &mut Camera,
+            &mut Transform,
+            &mut GlobalTransform,
+        )>,
+        cameras: Query<
+            (Entity, &Camera, &GlobalTransform),
+            (With<Skybox>, Without<SkyboxFollowCamera>),
+        >,
+    ) {
+        let mut all_cameras: HashMap<_, _> =
+            cameras.iter().map(|(e, c, gt)| (e, (c, gt))).collect();
+
+        // Update existing skybox cameras
+        for (entity, skybox_cam, mut camera, mut sky_cam_tfm, mut sky_cam_gtfm) in
+            &mut skybox_cameras
+        {
+            if let Some((followed_cam, followed_cam_transform)) =
+                all_cameras.remove(&skybox_cam.following)
+            {
+                // Update camera orientation
+                let (_, rotation, _) = followed_cam_transform.to_scale_rotation_translation();
+                *sky_cam_tfm = Transform {
+                    rotation,
+                    ..Transform::IDENTITY
+                };
+                *sky_cam_gtfm = sky_cam_tfm.compute_matrix().into();
+                camera.target = followed_cam.target.clone();
+                camera.viewport = followed_cam.viewport.clone();
+                camera.is_active = followed_cam.is_active;
+            } else {
+                // The camera we are trying to follow doesn't exist anymore!
+                commands.entity(entity).despawn_recursive();
+            }
+        }
+
+        // If there are any cameras left in `all_cameras`, they need to have a skybox camera added.
+        for (entity, (_, _)) in all_cameras.drain() {
+            println!("spawn sky follow");
+            commands.spawn((
+                SkyboxFollowCamera { following: entity },
+                Camera3dBundle {
+                    tonemapping: bevy::core_pipeline::tonemapping::Tonemapping::None,
+                    camera: Camera {
+                        order: SKYBOX_CAM_ORDER,
+                        ..default()
+                    },
+                    ..default()
+                },
+                RenderLayers::layer(SKYBOX_RENDER_LAYER),
+            ));
+        }
+    }
 }
